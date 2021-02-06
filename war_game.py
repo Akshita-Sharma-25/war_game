a304136ccdc33737be871acb1971f97305a42908
Akshita Sharma <akshita.2507.sharma@gmail.com>
2021-02-06 14:32:47 +0530

    added some new functionality

--- war_game.py
+++ war_game.py
@@ -32,9 +32,36 @@ class Deck:
 
     def deal_one(self):
         return self.cards.pop()
+    
+#player class
+class Player:
+    def __init__(self,name):
+        self.name=name
+        self.all_cards=[]
+    def deal(self):
+        return self.all_cards.pop()
+    def add(self,newcard):
+        if isinstance(newcard,list):
+            self.all_cards.extend(newcard)
+        else:
+            self.all_cards.append(newcard)
+    def __str__(self):
+        return f'the player {self.name} has {len(self.all_cards)} cards'
 
-deck = Deck()
+#game logic
+deck=Deck()
+#deck.show()
+n=len(deck.cards)
+#print(n//2)
+player1=Player('One')
+player2=Player('Two')
 deck.shuffle()
-deck.show()
-mycard = deck.deal_one()
-mycard.show()
+i=0
+for i in range(26):
+    player1.add(deck.deal_one())
+
+print(*[cards.show() for cards in player1.all_cards])
+
+
+
+
